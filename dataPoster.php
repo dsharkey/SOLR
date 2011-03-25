89117d97d2b376752ed0fd3aa989e264eb7204fe
djs22 <daniel.sharkey1@gmail.com>
2011-03-25 17:24:56 -0400

    dataPoster now posts all relevant articles in database. trying to figure out redirects in source urls

--- dataPoster.php
+++ dataPoster.php
@@ -1,4 +1,11 @@
 <?php
+
+function startsWith($haystack,$needle,$case=true) {
+    if($case){return (strcmp(substr($haystack, 0, strlen($needle)),$needle)===0);}
+    return (strcasecmp(substr($haystack, 0, strlen($needle)),$needle)===0);
+}
+
+
 $con = mysql_connect('localhost:8888', 'root', 'root');
     if (!$con)
       {
@@ -7,17 +14,22 @@ $con = mysql_connect('localhost:8888', 'root', 'root');
     mysql_select_db("newOnlineDiscourse", $con);
 
 
-$query = "SELECT * FROM cd_file_table WHERE filename like 'www%'";
+$query = "SELECT * FROM cd_file_table WHERE 'ignore'=0 AND id=36";
 
 $result = mysql_query($query);
 if (!$result) {
-    die('Invalid query: ' . mysql_error());
+    die('Invalid query: ' .$query." ". mysql_error());
 }
 $count=0;
 while($row = mysql_fetch_assoc($result)) {
-    $source = urlencode("http://".$row['filename']);
+    $source="";
+    if(startswith($row['filename'],"www"))
+        $source = urlencode("http://".$row['filename']);
+    else
+        $source = urlencode("http://www.".$row['filename']);
+
    // echo $source."<br>";
-    $desc=$row['description'];
+    $desc="literal.desc=".$row['description'];
     $title =urlencode($row['title']);
     $authors=$row['authors'];
     //need to split authors
@@ -35,7 +47,6 @@ while($row = mysql_fetch_assoc($result)) {
     $id = $row['id'];
 
     $url="http://localhost:8983/solr/update/extract?";
-    //literal.author=fakeAuthor&literal.title=fake&commit=true&uprefix=attr_&fmap.body=body&stream.url=http://www.duke.edu/web/aepi/" -d "literal.description=my Desc"
     $params="uprefix=attr_&literal.id=".$id."&literal.sup_title=".$title."&wt=json";
     foreach($authFin as $author){
         $params.="&literal.authors=".urlencode($author);
@@ -45,14 +56,16 @@ while($row = mysql_fetch_assoc($result)) {
     $url.=$params;
     $ch = curl_init($url);
 
+    echo $url."<br>";
     curl_setopt($ch, CURLOPT_POSTFIELDS, $desc);
+   // curl_setopt($ch,CURLOPT_AUTOREFERER,true);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
 
 
     $resp=json_decode(curl_exec($ch));
     if($resp->responseHeader->status!=0)
-            echo $id."<br>";
-    else echo"success".$id." ".$row['title']." "."<br>";
+            echo "fail ". $id."<br>";
+    else echo"success ".$id." ".$row['title']." "."<br>";
     curl_close($ch);
     $count++;
 }
---
+++ example/example-text-html/commit.sh
@@ -0,0 +1,3 @@
+cd ../exampledocs
+java -Ddata=args  -Durl=http://localhost:8983/solr/update -jar post.jar "<commit></commit>"
+cd ..
